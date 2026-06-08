d39570de575167a639ce43d46144720f3216d228
Will Runting <will.runting@gmail.com>
2026-06-08 10:52:55 +1000

    Add: wire live Apps Script endpoint into config

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 // Public by necessity (GitHub Pages free = public repo). Access control = unguessable
 // Apps Script URL + token. Worst case: stranger reads recipes / toggles pantry flags.
-export const API_URL = 'PASTE_APPS_SCRIPT_EXEC_URL';
-export const API_TOKEN = 'PASTE_TOKEN';
+export const API_URL = 'https://script.google.com/macros/s/AKfycbyhWaXfBHhdypskjy3cY2k-cwhGyy6V87ogCk0jCz8mg4bWLmkT5HgOY2sQ5RQFlpo0oQ/exec';
+export const API_TOKEN = '6fa5a26dee6f6320e544ff182b911d4576438edd8f9cd7cc';
